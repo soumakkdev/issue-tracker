2a5ddc2551acf03450ef012bb13dcfccf593350a
soumakkdev <soumakkdutta@gmail.com>
2023-11-18 10:25:38 +0530

    initial schema design

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,9 +1,13 @@
 import * as admin from 'firebase-admin'
 import { getAuth } from 'firebase-admin/auth'
 
-const app = admin.initializeApp({
-	credential: admin.credential.cert('./serviceAccountKey.json'),
-})
+let app
+
+if (!admin.apps.length) {
+	app = admin.initializeApp({
+		credential: admin.credential.cert('./serviceAccountKey.json'),
+	})
+}
 
 const auth = getAuth(app)
 
